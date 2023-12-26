3ffe5d45d4e26b2a1ebc7459626b1cbcebc74581
Muhammad Daffa Fachreza <113222238+xsillery12@users.noreply.github.com>
2023-12-26 23:13:49 +0700

    Delete raakdatri-web directory

--- raakdatri-web/components/admin_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "../database/connect.php";
-
-setcookie('admin_id','', time() - 1, '/');
-header('location:../admin/admin_login.php');
-
-?>
--- raakdatri-web/components/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-    <p>&copy; copyright @ <?= date('Y') ?> by <span>Raakdatri Team</span> | all rights reserved!</p>
-</footer>
